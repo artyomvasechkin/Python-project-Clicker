22b1cd492761da0d66bd2a321cad344a9a8f32f7
Andrew <47502027+artyomvasechkin@users.noreply.github.com>
2021-04-29 15:59:38 +0300

    Update Curency_class.py

--- web/src/Curency_class.py
+++ web/src/Curency_class.py
@@ -2,16 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
+
 class Curency:
-    Tesla = 'https://www.google.ru/search?q=%D1%86%D0%B5%D0%BD%D0%B0+%D0%B0%D0%BA%D1%86%D0%B8%D0%B8+%D1%82%D0%B5%D1%81%D0%BB%D0%B0&newwindow=1&safe=strict&sxsrf=ALeKk02vRIY0BD5Opvl7_RH6i9SI4xnXew%3A1618656421081&source=hp&ei=pLx6YM7DN-eIrwT9uKH4DQ&iflsig=AINFCbYAAAAAYHrKteOE94HOc6XPdOIP8X1JamlA8KCU&oq=%D1%86%D0%B5%D0%BD%D0%B0+%D0%B0%D0%BA%D1%86%D0%B8%D0%B8+&gs_lcp=Cgdnd3Mtd2l6EAEYADIHCAAQRhD6ATICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADoFCAAQsQM6CwgAELEDEMcBEK8BOggILhCxAxCDAToICAAQsQMQgwE6CwgAELEDEMcBEKMCOgUILhCxAzoICAAQsQMQyQM6CAgAEMcBEK8BOg4IABCxAxCDARDHARCjAjoKCAAQsQMQRhCCAjoFCAAQyQNQqVpY0G9g-oABaABwAHgAgAFOiAGxBZIBAjExmAEAoAEBqgEHZ3dzLXdpeg&sclient=gws-wiz'
+    Tesla = 'https://www.google.ru/search?q=%D1%86%D0%B5%D0%BD%D0%B0+%D0%B0%D0%BA%D1%86%D0%B8%D0%B8+%D1%82%D0%B5%D1' \
+            '%81%D0%BB%D0%B0&newwindow=1&safe=strict&sxsrf=ALeKk02vRIY0BD5Opvl7_RH6i9SI4xnXew%3A1618656421081&source' \
+            '=hp&ei=pLx6YM7DN-eIrwT9uKH4DQ&iflsig=AINFCbYAAAAAYHrKteOE94HOc6XPdOIP8X1JamlA8KCU&oq=%D1%86%D0%B5%D0%BD' \
+            '%D0%B0+%D0%B0%D0%BA%D1%86%D0%B8%D0%B8+&gs_lcp' \
+            '=Cgdnd3Mtd2l6EAEYADIHCAAQRhD6ATICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADoFCAAQsQM6CwgAELEDEMcBEK8BOggILhCxAxCDAToICAAQsQMQgwE6CwgAELEDEMcBEKMCOgUILhCxAzoICAAQsQMQyQM6CAgAEMcBEK8BOg4IABCxAxCDARDHARCjAjoKCAAQsQMQRhCCAjoFCAAQyQNQqVpY0G9g-oABaABwAHgAgAFOiAGxBZIBAjExmAEAoAEBqgEHZ3dzLXdpeg&sclient=gws-wiz '
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0'}
 
     current_converted_price = 0
-    difference = 0.1
-    first_time = True
+    buy_price = 0
+    sell_price = 0
 
     def __init__(self):
         self.current_converted_price = float(self.get_currency_price().replace(",", "."))
+        print("Укажите цену, за которую хотите купить акцию: ")
+        self.buy_price = float(input())
+        print("Укажите цену, за которую хотите продать акцию: ")
+        self.sell_price = float(input())
 
     def get_currency_price(self):
         full_page = requests.get(self.Tesla, headers=self.headers)
@@ -21,14 +30,17 @@ class Curency:
 
     def check_currency(self):
         currency = float(self.get_currency_price().replace(",", "."))
-        if self.first_time:
-            print("1 акция Теслы стоит " + str(currency) + "$")
-            self.first_time = False
 
-        if currency >= self.current_converted_price + self.difference:
+        if currency >= self.sell_price:
             print("1 акция Теслы стоит " + str(currency) + "$")
-        elif currency <= self.current_converted_price - self.difference:
-            print("1 акция Теслы стоит " + str(currency) + "$")
+            print("ПРОДАВАЙТЕ")
+            return
 
-        time.sleep(3)
+        elif currency <= self.buy_price:
+            print("1 акция Теслы стоит " + str(currency) + "$")
+            print("ПОКУПАЙТЕ")
+            return
+
+        print("1 акция Теслы стоит " + str(currency) + "$")
+        time.sleep(20)
         self.check_currency()
